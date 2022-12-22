75bf2fc16e6b6b22230e32a20760bce06bfc463b
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-12-22 15:20:54 +0530

    feat: read userName, store in local storage, conditionally render greetUser component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { GetUserName } from "./components/GetUserName";
 import { GreetUser } from "./components/GreetUser";
 
 function App() {
+  const userNameOnInitialRender = () => localStorage.getItem("userName") ?? "";
+
+  const [userName, setUserName] = useState(userNameOnInitialRender());
+
+  useEffect(() => {
+    localStorage.setItem("userName", userName);
+  }, [userName]);
+
   return (
     <div className="app-container">
       <div className="app">
-        {/* <GetUserName /> */}
-        <GreetUser />
+        {userName ? <GreetUser /> : <GetUserName setUserName={setUserName} />}
       </div>
     </div>
   );
--- src/components/GetUserName/index.jsx
+++ src/components/GetUserName/index.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./GetUserName.css";
 
-const GetUserName = () => {
+const GetUserName = ({ setUserName }) => {
+  const [userNameInput, setUserNameInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserName(userNameInput);
+  };
   return (
-    <main className="get-user-name">
+    <form onSubmit={handleSubmit} className="get-user-name">
       <p>Hello, What's your name ?</p>
-      <input className="get-user-name-input" />
-      <button className="btn btn-primary-outline">Continue</button>
-    </main>
+      <input
+        value={userNameInput}
+        onChange={(e) => setUserNameInput(e.target.value)}
+        className="get-user-name-input"
+      />
+      <button type="submit" className="btn btn-primary-outline">
+        Continue <i className="fa-solid fa-angle-right"></i>
+      </button>
+    </form>
   );
 };
 
